8ad243ca7dab5f70b3b310fb350871568c3be810
FreddyBicandy50 <fredos112033@gmail.com>
2023-01-04 11:41:51 +0200

    Front-end

---
+++ Webdev/University/Project/site.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head> 
+    <link rel="stylesheet"type="text/css"
+            href="file://wsl.localhost/kali-linux/home/null/programing/Webdev/University/Project/CSS/header.css">
+        <link rel="stylesheet" type="text/css"
+            href="file://wsl.localhost/kali-linux/home/null/programing/Webdev/University/Project/CSS/home.css">
+        <link rel="stylesheet" type="text/css"
+            href="file://wsl.localhost/kali-linux/home/null/programing/Webdev/University/Project/CSS/slider.css">
+        <script src="file://wsl.localhost/kali-linux/home/null/programing/Webdev/University/Project/JS/script.js"
+            defer></script>
+        <script src="file://wsl.localhost/kali-linux/home/null/programing/Webdev/University/Project/JS/slider.js"
+            defer></script>
+        <link rel="stylesheet" type="text/css" href="https://unpkg.com/aos@next/dist/aos.css" />
+ 
+</head>
+
+  
+<body>
+  
+        <?php 
+            include "Bicandy's Network Solutions.html" ;
+            include "img/logo.png" ;
+        ?>
+        <style>
+            <?php     
+                include "CSS/header.css";
+                include "CSS/home.css";
+                include "CSS/slider.css";  
+            ?>
+        </style> 
+        
+        <script>
+            <?php
+                include "JS/script.js";
+                include "JS/slider.js";
+            ?>
+        </script> 
+</body>
+</html>
