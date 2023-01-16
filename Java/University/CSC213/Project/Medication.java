1968039b18fb5c821788bab8056dced246eb0afb
FreddyBicandy50 <fredos112033@gmail.com>
2023-01-17 00:28:34 +0200

    Save

--- Java/University/CSC213/Project/Medication.java
+++ Java/University/CSC213/Project/Medication.java
@@ -1,13 +1,8 @@
 package Project;  
 public class Medication { 
     public static void main(String[] args){   
-        new admin_panel();   
+        new login();   
     } 
-
-
-
-}
-
  
-
+}  
 
