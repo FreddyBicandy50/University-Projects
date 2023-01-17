4d54c5714c81dc1aae167bcf1b46e19b68f3f3e1
FreddyBicandy50 <fredos112033@gmail.com>
2023-01-17 15:14:47 +0200

    Save

--- Java/University/CSC213/Project/GIUI/add_records.java
+++ Java/University/CSC213/Project/GIUI/add_records.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import Project.OOP.DayShift;
 import Project.OOP.Doctor;
 import Project.OOP.NightShift;
+import Project.OOP.SurgeryP;
 import Project.OOP.TreatmentP;
 
 import java.awt.event.ActionEvent;
@@ -44,6 +45,7 @@ public class add_records  {
         ADD_Doctor();
         ADD_Nurse();
         ADD_Ptreatments();
+        ADD_PSurgery();
        // ADD_Patient_Surgery();
         frame.setVisible(true);
         
@@ -56,40 +58,40 @@ public class add_records  {
         nameLabel.setBounds(20, 50, 80, 25);
         panel.add(nameLabel);
         // name Field input box
-         JTextField  nameField = new JTextField(20);
-         nameField.setText("");
-         nameField.setBounds(100, 50, 150, 25);
-         panel.add( nameField);
+        JTextField  nameField = new JTextField(20);
+        nameField.setText("");
+        nameField.setBounds(100, 50, 150, 25);
+        panel.add( nameField);
 
         // @age Field label
         JLabel  ageLabel = new JLabel("Age:");
-         ageLabel.setBounds(20, 80, 80, 25);
+        ageLabel.setBounds(20, 80, 80, 25);
         panel.add( ageLabel);
         // age Field input box
         JTextField  ageField = new JTextField(20);
-         ageField.setBounds(100, 80, 150, 25);
-         ageField.setText("");
+        ageField.setBounds(100, 80, 150, 25);
+        ageField.setText("");
         panel.add( ageField);
 
         // @id Field label
         JLabel  idLabel = new JLabel("Id:");
-         idLabel.setBounds(20, 110, 80, 25);
+        idLabel.setBounds(20, 110, 80, 25);
         panel.add( idLabel);
         // id Field input box
         JTextField  idField = new JTextField(20);
-         idField.setBounds(100, 110, 150, 25);
-         idField.setText("");
+        idField.setBounds(100, 110, 150, 25);
+        idField.setText("");
         panel.add( idField);
 
         // @speciality Field Label
         JLabel  specLabel = new JLabel("Speciality:");
-         specLabel.setBounds(20, 140, 80, 25);
+        specLabel.setBounds(20, 140, 80, 25);
         panel.add( specLabel);
         // speciality Field input box
         JTextField  specField = new JTextField(20);
-         specField.setBounds(100, 140, 150, 25);
-         specField.setText("");
-            panel.add( specField);
+        specField.setBounds(100, 140, 150, 25);
+        specField.setText("");
+        panel.add( specField);
 
         // Add doctor button
         JButton add_btn = new JButton("Add Doctor");
@@ -135,60 +137,60 @@ public class add_records  {
         // ADD A NURSE
         // @name Field label
         JLabel  nameLabel = new JLabel("Name:");
-        nameLabel.setBounds(550, 20, 80, 25);
+        nameLabel.setBounds(550, 10, 80, 25);
         panel.add(nameLabel);
         // name Field input box
         JTextField nameField = new JTextField(20);
         nameField.setText("");
-        nameField.setBounds(660, 20, 150, 25);
+        nameField.setBounds(660, 10, 150, 25);
         panel.add(nameField);
 
         // @age Field label
         JLabel ageLabel = new JLabel("Age:");
-        ageLabel.setBounds(550, 50, 80, 25);
+        ageLabel.setBounds(550, 40, 80, 25);
         panel.add(ageLabel);
         // age Field input box
         JTextField ageField = new JTextField(20);
-        ageField.setBounds(660, 50, 150, 25);
+        ageField.setBounds(660, 40, 150, 25);
         ageField.setText("");
         panel.add(ageField);
 
         // @id Field label
         JLabel  idLabel = new JLabel("Id:");
-        idLabel.setBounds(550, 80, 80, 25);
+        idLabel.setBounds(550, 70, 80, 25);
         panel.add(idLabel);
         // id Field input box
         JTextField  idField = new JTextField(20);
-        idField.setBounds(660, 80, 150, 25);
+        idField.setBounds(660, 70, 150, 25);
         idField.setText("");
         panel.add(idField);
 
         // @Department Field Label
         JLabel  DepartmentLabel = new JLabel("Department:");
-        DepartmentLabel.setBounds(550, 110, 90, 25);
+        DepartmentLabel.setBounds(550, 100, 90, 25);
         panel.add(DepartmentLabel);
         // Department Field input box
         JTextField DepartmentField = new JTextField(20);
-        DepartmentField.setBounds(660, 110, 150, 25);
+        DepartmentField.setBounds(660, 100, 150, 25);
         DepartmentField.setText("");
         panel.add(DepartmentField);
 
         // @time Field Label
         JLabel  timeLabel = new JLabel("Time:");
-        timeLabel.setBounds(550, 140, 90, 25);
+        timeLabel.setBounds(550, 130, 90, 25);
         panel.add(timeLabel);
         // time Field input box
         JTextField  timeField = new JTextField(20);
-        timeField.setBounds(660, 140, 150, 25);
+        timeField.setBounds(660, 130, 150, 25);
         timeField.setText("");
         panel.add(timeField);
 
         // @Shift Field Label
         JLabel  ShiftLabel = new JLabel("Shift:");
-        ShiftLabel.setBounds(550, 170, 90, 25);
+        ShiftLabel.setBounds(550, 160, 90, 25);
         panel.add(ShiftLabel);
         // Shift Field input box 
-        ShiftField.setBounds(660, 170, 150, 25); 
+        ShiftField.setBounds(660, 160, 150, 25); 
         panel.add(ShiftField);
         ShiftField.addFocusListener(new FocusListener() {
             @Override
@@ -206,12 +208,12 @@ public class add_records  {
         });
         // Add nurse button
         JButton add_btn = new JButton("Add Nurse");
-        add_btn.setBounds(600, 210, 120, 25);
+        add_btn.setBounds(600, 190, 120, 25);
         panel.add(add_btn);
         // On click @add_dr_Button
         add_btn.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                message.setBounds(600, 240, 600, 25);
+                message.setBounds(600, 220, 600, 25);
                 // if One of the textfileds are empty display an error message
                 if (nameField.getText().isEmpty() ||
                         ageField.getText().equals("") ||
@@ -260,78 +262,78 @@ public class add_records  {
         // ADD Patients On treatments
         // @name Field label
         JLabel  nameLabel = new JLabel("Name:");
-        nameLabel.setBounds(10, 255, 80, 25); 
+        nameLabel.setBounds(20, 255, 80, 25); 
         panel.add(nameLabel);
         // name Field input box
         JTextField nameField = new JTextField(20);
         nameField.setText("");
-        nameField.setBounds(60, 255, 150, 25);
+        nameField.setBounds(100, 255, 150, 25);
         panel.add(nameField);
 
         // @age Field label
         JLabel ageLabel = new JLabel("Age:");
-        ageLabel.setBounds(10, 285, 80, 25);
+        ageLabel.setBounds(20, 285, 80, 25);
         panel.add(ageLabel);
         // age Field input box
         JTextField ageField = new JTextField(20);
-        ageField.setBounds(60, 285, 150, 25);
+        ageField.setBounds(100, 285, 150, 25);
         ageField.setText("");
         panel.add(ageField);
        
         // @id Field label
         JLabel idLabel = new JLabel("Id:");
-        idLabel.setBounds(10, 315, 80, 25);
+        idLabel.setBounds(20, 315, 80, 25);
         panel.add(idLabel);
         // id Field input box
         JTextField idField = new JTextField(20);
-        idField.setBounds(60, 315, 150, 25);
+        idField.setBounds(100, 315, 150, 25);
         idField.setText("");
         panel.add(idField);
        
          // @Date Field Label
          JLabel Date = new JLabel("Date:");
-         Date.setBounds(10, 345, 90, 25);
+         Date.setBounds(20, 345, 90, 25);
          panel.add(Date);
          // Date Field input box
          JTextField dateField = new JTextField(20);
-         dateField.setBounds(60, 345, 150, 25);
+         dateField.setBounds(100, 345, 150, 25);
          dateField.setText("");
          panel.add(dateField);
        
          // @time Field Label
          JLabel timeLabel = new JLabel("Time:");
-         timeLabel.setBounds(10, 375, 90, 25);
+         timeLabel.setBounds(20, 375, 90, 25);
          panel.add(timeLabel);
          // time Field input box
          JTextField timeField = new JTextField(20);
-         timeField.setBounds(60, 375, 150, 25);
+         timeField.setBounds(100, 375, 150, 25);
          timeField.setText("");
          panel.add(timeField);
 
 
          // @time Field Label
          JLabel TreatmentLabel = new JLabel("Treatment:");
-         TreatmentLabel.setBounds(10, 405, 90, 25);
+         TreatmentLabel.setBounds(20, 405, 90, 25);
          panel.add(TreatmentLabel);
          // time Field input box
          JTextField treatmentField = new JTextField(20);
-         treatmentField.setBounds(90, 405, 150, 25);
+         treatmentField.setBounds(100, 405, 150, 25);
          treatmentField.setText(""); 
          panel.add(treatmentField); 
         
           // Add Patient button
-          JButton add_btn = new JButton("Add PTreatment");
-          add_btn.setBounds(50, 435, 200, 25);
+          JButton add_btn = new JButton("Add Patient On Treatment");
+          add_btn.setBounds(20, 435, 270, 25);
           panel.add(add_btn);
           // On click @add_dr_Button
           add_btn.addActionListener(new ActionListener() {
              public void actionPerformed(ActionEvent e) {
-                    message.setBounds(50, 465, 600, 25);
-                    // if One of the textfileds are empty display an error message
-                    if (nameField.getText().isEmpty() || ageField.getText().equals("") ||
+                 // if One of the textfileds are empty display an error message
+                 if (nameField.getText().isEmpty() || ageField.getText().equals("") ||
                         idField.getText().equals("") || dateField.getText().equals("") ||
                         timeField.getText().equals("") || treatmentField.getText().equals("")) {
                         // display error message
+                        message.setBounds(100, 465, 600, 25);
                         message.setText("empty Fields");
                     }else { 
                         // get values from text fields
@@ -344,16 +346,119 @@ public class add_records  {
                         // Add the doctor to the array database 
                         login.db.add(new TreatmentP(name, age, id, date, time, treatment)); 
                         // Display success message
+                        message.setBounds(20, 465, 600, 25);
                         message.setText("Patient Added Successfully! " + name);
+                        // reset all fileds
+                        nameField.setText("");
+                        ageField.setText("");
+                        idField.setText("");
+                        dateField.setText("");
+                        timeField.setText("");
+                        treatmentField.setText("");
                     }
-                    // reset all fileds
-                    nameField.setText("");
-                    ageField.setText("");
-                    idField.setText("");
-                    dateField.setText("");
-                    timeField.setText("");
-                    treatmentField.setText(""); 
+                    
            
+            }  
+          }); 
+    }
+    public void ADD_PSurgery(){
+        // ADD Patients On treatments
+        // @name Field label
+        JLabel  nameLabel = new JLabel("Name:");
+        nameLabel.setBounds(550, 255, 80, 25); 
+        panel.add(nameLabel);
+        // name Field input box
+        JTextField nameField = new JTextField(20);
+        nameField.setText("");
+        nameField.setBounds(660, 255, 150, 25);
+        panel.add(nameField);
+
+        // @age Field label
+        JLabel ageLabel = new JLabel("Age:");
+        ageLabel.setBounds(550, 285, 80, 25);
+        panel.add(ageLabel);
+        // age Field input box
+        JTextField ageField = new JTextField(20);
+        ageField.setBounds(660, 285, 150, 25);
+        ageField.setText("");
+        panel.add(ageField);
+       
+        // @id Field label
+        JLabel idLabel = new JLabel("Id:");
+        idLabel.setBounds(550, 315, 80, 25);
+        panel.add(idLabel);
+        // id Field input box
+        JTextField idField = new JTextField(20);
+        idField.setBounds(660, 315, 150, 25);
+        idField.setText("");
+        panel.add(idField);
+       
+         // @Date Field Label
+         JLabel Date = new JLabel("Date:");
+         Date.setBounds(550, 345, 90, 25);
+         panel.add(Date);
+         // Date Field input box
+         JTextField dateField = new JTextField(20);
+         dateField.setBounds(660, 345, 150, 25);
+         dateField.setText("");
+         panel.add(dateField);
+       
+         // @time Field Label
+         JLabel timeLabel = new JLabel("Time:");
+         timeLabel.setBounds(550, 375, 90, 25);
+         panel.add(timeLabel);
+         // time Field input box
+         JTextField timeField = new JTextField(20);
+         timeField.setBounds(660, 375, 150, 25);
+         timeField.setText("");
+         panel.add(timeField);
+
+
+         // @time Field Label
+         JLabel SurgerLabel = new JLabel("Surgery Type:");
+         SurgerLabel.setBounds(550, 405, 130, 25);
+         panel.add(SurgerLabel);
+         // time Field input box
+         JTextField SrugeryField = new JTextField(20);
+         SrugeryField.setBounds(660, 405, 150, 25);
+         SrugeryField.setText(""); 
+         panel.add(SrugeryField); 
+        
+          // Add Patient button
+          JButton add_btn = new JButton("Add Patient Made Surgery");
+          add_btn.setBounds(550, 435, 250, 25);
+          panel.add(add_btn);
+          // On click @add_Button
+          add_btn.addActionListener(new ActionListener() {
+             public void actionPerformed(ActionEvent e) {
+                 // if One of the textfileds are empty display an error message
+                 if (nameField.getText().isEmpty() || ageField.getText().equals("") ||
+                 idField.getText().equals("") || dateField.getText().equals("") ||
+                 timeField.getText().equals("") || SrugeryField.getText().equals("")) {
+                     // display error message
+                        message.setBounds(650, 465, 600, 25);
+                        message.setText("empty Fields");
+                    }else { 
+                        // get values from text fields
+                        String name = nameField.getText();
+                        String age = ageField.getText();
+                        int id = Integer.parseInt(idField.getText());
+                        String date = dateField.getText();
+                        String time = timeField.getText();
+                        String Surgery = SrugeryField.getText();
+                        // Add the doctor to the array database 
+                        login.db.add(new SurgeryP(name, age, id, date, time, Surgery)); 
+                        // Display success message
+                        message.setBounds(550, 465, 600, 25);
+                        message.setText("Patient Added Successfully! " + name);
+                        // reset all fileds
+                        nameField.setText("");
+                        ageField.setText("");
+                        idField.setText("");
+                        dateField.setText("");
+                        timeField.setText("");
+                        SrugeryField.setText("");
+                    } 
             }  
           }); 
     }
