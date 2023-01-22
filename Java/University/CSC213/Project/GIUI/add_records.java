7302909b410252fcb94029cad89ede70773222b8
FreddyBicandy50 <fredos112033@gmail.com>
2023-01-22 21:18:38 +0200

    Save

--- Java/University/CSC213/Project/GIUI/add_records.java
+++ Java/University/CSC213/Project/GIUI/add_records.java
@@ -34,13 +34,16 @@ public class add_records  {
         //  Calling Button Function 
         init.goback_btn();
     }
-    public void AddDoctor(){    
+    public void AddDoctor(){ 
+        // Fields Position   
         init.specLabel.setBounds(init.x, 140, 80, 25);  
         init.specField.setBounds(init.xf, 140, 150, 25);  
         init.Add_info.setBounds(init.x+20, 170, 120, 25);
+        // Add Button
         init.Add_btn(1);
     }
     public void AddNurse(){ 
+        // Fields Position
         init.DepartmentLabel.setBounds(init.x, 140, 90, 25);
         init.timeLabel.setBounds(init.x, 170, 90, 25); 
         init.ShiftLabel.setBounds(init.x, 200, 90, 25);  
@@ -48,9 +51,11 @@ public class add_records  {
         init.timeField.setBounds(init.xf, 170, 150, 25);   
         init.ShiftField.setBounds(init.xf, 200, 150, 25);  
         init.Add_info.setBounds(init.x + 20, 230, 120, 25); 
+        // Add Button
         init.Add_btn(2);
     }
     public void AddPtreatments(){  
+            // Fields Postions
             init.DateLabel.setBounds(init.x, 140, 90, 25);  
             init.timeLabel.setBounds(init.x, 170, 90, 25);  
             init.TreatmentLabel.setBounds(init.x, 200, 90, 25); 
@@ -58,20 +63,21 @@ public class add_records  {
             init.timeField.setBounds(init.xf, 170, 150, 25); 
             init.treatmentField.setBounds(init.xf, 200, 150, 25); 
             init.Add_info.setBounds(init.xf, 230, 120, 25);  
+            // Add Button
             init.Add_btn(3);
     }
     public void AddPSurgery(){ 
-       
+            // Fields Positions
             init.DateLabel.setBounds(init.x, 140, 90, 25);
             init.timeLabel.setBounds(init.x, 170, 90, 25);
             init.TreatmentLabel.setBounds(init.x, 200, 90, 25);
             init.DateField.setBounds(init.xf, 140, 150, 25);
             init.timeField.setBounds(init.xf, 170, 150, 25);
-            init.treatmentField.setBounds(init.xf, 200, 150, 25);
-
+            init.treatmentField.setBounds(init.xf, 200, 150, 25); 
             init.SrugeryLabel.setBounds(init.x, 230, 130, 25);   
             init.SrugeryField.setBounds(init.xf, 230, 150, 25);  
             init.Add_info.setBounds(init.xf, 260, 120, 25);  
+            // Add Button
             init.Add_btn(4);
 
 }
