363d36df21e84cb090bf51271ce29fd9909e1f13
FreddyBicandy50 <fredos112033@gmail.com>
2023-03-01 23:19:24 +0200

    publishd

---
+++ java/create_GIT_repository.sh
@@ -0,0 +1,7 @@
+echo "# Telecom_Managment_System" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:FreddyBicandy50/Telecom_Managment_System.git
+git push -u origin main
