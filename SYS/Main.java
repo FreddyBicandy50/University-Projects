8fd8b719beead1ca21e723d391b653178ab31840
FreddyBicandy50 <fredos112033@gmail.com>
2023-06-28 16:32:51 +0300

    push

---
+++ SYS/Main.java
@@ -0,0 +1,4 @@
+import GIUI.*;  
+public class Main { 
+    public static void main(String[] args){ new login();}
+} 
---
+++ SYS/OOP/Hospital.java
@@ -0,0 +1,2 @@
+package OOP;
+public interface Hospital {public abstract String getMedication();}
